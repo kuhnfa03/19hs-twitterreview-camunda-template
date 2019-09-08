023c0ea736d2b97ed78a581c5aabe7b07a4e16b9
Björn Scheppler <scep@zhaw.ch>
2019-09-08 13:27:16 +0200

    Refactoring

--- src/main/java/ch/zhaw/gpi/trmusterloesung/CamundaProcessApplication.java
+++ src/main/java/ch/zhaw/gpi/trmusterloesung/CamundaProcessApplication.java
@@ -1,4 +1,4 @@
-package ch.zhaw.gpi.csbtemplate;
+package ch.zhaw.gpi.trmusterloesung;
 
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
